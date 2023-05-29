ba243c54b4efe474540c134424a91374bc232320
Daniel Bell <daniel.bell96@hotmail.co.uk>
2023-05-29 22:38:48 +0100

    3D Scatter Animation

--- dashboard/code/figures.py
+++ dashboard/code/figures.py
@@ -1,6 +1,7 @@
 import os
 import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 import datetime
 from plotly_calplot import calplot
 
@@ -268,6 +269,46 @@ def scatter3D(df):
     fig.update_layout(
         layouts.scatter3d(variable, units)
     )
+
+    # Use for animation rotation
+    # https://github.com/Coding-with-Adam/Dash-by-Plotly/blob/master/Plotly_Graphs/3d_scatter/scatter3d.py
+    x_eye = 1.25
+    y_eye = 2
+    z_eye = 0
+
+    fig.update_layout(
+        scene_camera_eye=dict(x=x_eye, y=y_eye, z=z_eye),
+        updatemenus=[dict(
+            type='buttons',
+            showactive=False,
+            y=1,
+            x=0.8,
+            xanchor='left',
+            yanchor='bottom',
+            pad=dict(t=45, r=10),
+            buttons=[dict(
+                label='Play',
+                method='animate',
+                args=[None, dict(
+                    frame=dict(duration=100, redraw=True),
+                    transition=dict(duration=0),
+                    fromcurrent=True,
+                    mode='immediate'
+                )]
+            )]
+        )]
+    )
+
+    def rotate_z(x, y, z, theta):
+        w = x + 1j * y
+        return np.real(np.exp(1j*theta)*w), np.imag(np.exp(1j*theta)*w), z
+
+    frames = []
+
+    for t in np.arange(0, 6.26, 0.1):
+        xe, ye, ze = rotate_z(x_eye, y_eye, z_eye, -t)
+        frames.append(go.Frame(layout=dict(scene_camera_eye=dict(x=xe, y=ye, z=ze))))
+    fig.frames = frames
 
     return fig
 
